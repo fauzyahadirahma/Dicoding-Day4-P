6773c5a85bf4b2468fdce40cef947e2b642b1352
Fauzyah Hadirahma <97279376+fauzyahadirahma@users.noreply.github.com>
2022-01-26 01:55:18 +0700

    Pengisian Nilai dengan Inisialisa, 12 Januari 2022

---
+++ main.c
@@ -0,0 +1,12 @@
+/* Penulis   : Fauzyah Hadirahma, email fauzyah.hadirahma@gmail.com */
+/* Deskripsi : Pengisian Nilai dengan Inisialisasi */
+
+int main() {
+    /* KAMUS */
+    int i = 5; /* deklarasi dan inisialisasi nilai variable i dengan 5 */
+    
+    /* ALGORITMA */
+    printf ("Ini nilai i : %d \n", i); /* untuk mengamati dampak inisialisasi */
+    
+    return 0;
+}
